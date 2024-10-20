7c577241700112af279eb6acc33864b93c2c0fcc
Ashutosh Mishra <am701567@gmail.com>
2024-10-20 12:01:16 +0530

    Ep 2 Ignite  App withe Node configuration

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+const parent=React.createElement("div",{id:"parent"},
+   React.createElement("div",{id:"child1"},
+    React.createElement("h1",{id:"h1style"},'Namaste React by React-H1 🫡'),
+    React.createElement("h2",{id:"h1style"},'Namaste React by React-H2')
+   ),
+   React.createElement("div",{id:"child2"},
+    React.createElement("h3",{id:"h1style"},'Namaste React by React-H3'),
+    React.createElement("h4",{id:"h1style"},'Namaste React by React-H4')
+   ))
+console.log(parent)
+
+const root=ReactDOM.createRoot(document.getElementById("root")) 
+root.render(parent)
---
+++ README.md
@@ -0,0 +1,17 @@
+# Parcel
+
+* Dev Build
+* Create Local Server
+* HMR=Hot Module Replacment
+* File Watching Algorithm-written in C++
+* Caching-For faster Builds
+* Image Optimization
+* Minification
+* Bundling
+* Compressing
+* Consistent Hashing
+* Code Spliting
+* Differential Bundling-to support older browser
+* Diagnostice
+* Error Handling
+* Tree Shaking
