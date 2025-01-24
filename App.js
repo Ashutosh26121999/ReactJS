1e99c2ce907e180bd4497319b9f8b2149cc74f33
Ashutosh Mishra <am701567@gmail.com>
2025-01-24 18:13:12 +0530

    Minor changes in Contact us page

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import {RouterProvider} from "react-router-dom";
 import {routerConfig} from "./src/Utils/routes/routerConfig.js";
 import {Provider} from "react-redux";
 import store from "./src/Rduex/store.js";
+import {Toaster} from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <RouterProvider router={routerConfig} />
+    <Toaster position='top-center' />
   </Provider>,
 );
